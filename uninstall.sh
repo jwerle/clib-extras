02e009bf522da5b60a715de94071682ccbd6ff8d
Joseph Werle <joseph.werle@gmail.com>
2014-05-16 15:06:18 -0400

    rm'd completion when uninstalling

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+## completion file
+COMPLETION_FILE="${HOME}/.clib-completions.sh"
+
+## ensure there is something to uninstall
 if [ -z "${REPOS}" ]; then
   {
     echo "Nothing to uninstall"
@@ -7,10 +11,12 @@ if [ -z "${REPOS}" ]; then
   } >&2
 fi
 
+## convert to array
 REPOS=($(echo ${REPOS}))
 let len=${#REPOS[@]}
 let i=0
 
+## ensure command exists
 if ! type -f clib-uninstall > /dev/null 2>&1; then
   {
     echo "Missing \`clib-uninstall'"
@@ -18,6 +24,10 @@ if ! type -f clib-uninstall > /dev/null 2>&1; then
   }
 fi
 
+## uninstall
 for ((i = 0; i < len; ++i)); do
   clib uninstall ${REPOS[$i]}
 done
+
+## remove completion file
+rm -f "${COMPLETION_FILE}"
